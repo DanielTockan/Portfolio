7c047ea7864082e58088a72fbb773db0ba335a3b
danielzt94 <danieltockan@gmail.com>
2021-01-07 00:33:52 +0000

    Uploaded to GitHub

---
+++ build/precache-manifest.6d49ef17b6ff2ad88bd8800743245000.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a045280f9fdcacd4e5e3011f60bae31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e4af4099aa5049a2dee",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "3eaad955294fe1d347b9",
+    "url": "/static/js/2.6c70cb2b.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.6c70cb2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e4af4099aa5049a2dee",
+    "url": "/static/js/main.400fc56d.chunk.js"
+  },
+  {
+    "revision": "63738b3e0669c03ad231",
+    "url": "/static/js/runtime-main.9e7f8d4d.js"
+  },
+  {
+    "revision": "a9dd8fcf6cc23d8a7e1a075c24879cfb",
+    "url": "/static/media/danieltockan2.a9dd8fcf.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
