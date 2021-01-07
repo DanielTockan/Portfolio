6a9833b89422d36ad87193cac92d02723415a6d6
danielzt94 <danieltockan@gmail.com>
2021-01-07 00:53:14 +0000

    Updates

--- precache-manifest.9dcf60b57ef65a532889adfc6ff17bb5.js
+++ precache-manifest.9dcf60b57ef65a532889adfc6ff17bb5.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2a045280f9fdcacd4e5e3011f60bae31",
+    "revision": "b2ac9ab1d7b0b6e247160fd9b8a62d0d",
     "url": "/index.html"
   },
   {
-    "revision": "5e4af4099aa5049a2dee",
+    "revision": "a3e1b002e325ece039ec",
     "url": "/static/css/main.f5497eb8.chunk.css"
   },
   {
-    "revision": "3eaad955294fe1d347b9",
-    "url": "/static/js/2.6c70cb2b.chunk.js"
+    "revision": "a0294a0f1fbd050c0c4b",
+    "url": "/static/js/2.f6a2454b.chunk.js"
   },
   {
     "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
-    "url": "/static/js/2.6c70cb2b.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.f6a2454b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5e4af4099aa5049a2dee",
-    "url": "/static/js/main.400fc56d.chunk.js"
+    "revision": "a3e1b002e325ece039ec",
+    "url": "/static/js/main.ea992fdb.chunk.js"
   },
   {
-    "revision": "63738b3e0669c03ad231",
-    "url": "/static/js/runtime-main.9e7f8d4d.js"
+    "revision": "a4e8eef93e6c7b28f246",
+    "url": "/static/js/runtime-main.c50f5db5.js"
   },
   {
     "revision": "a9dd8fcf6cc23d8a7e1a075c24879cfb",
