1075cdb8c62a60b91976009c3f24a9f2662c5104
danielzt94 <danieltockan@gmail.com>
2021-01-07 00:52:01 +0000

    Uploaded to GitHub

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/DanielTockan/Portfolio";
+const repoURL = "https://github.com/DanielTockan/Portfolio.git";
 
 ghpages.publish(
   pathname,
